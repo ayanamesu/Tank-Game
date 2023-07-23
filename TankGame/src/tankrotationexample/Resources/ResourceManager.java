c0b262f3d4df524d0be7c72bc8d516aeefe4f339
ayanamesu <leejacky74@gmail.com>
2023-07-22 21:45:06 -0700

    resources

---
+++ TankGame/src/tankrotationexample/Resources/ResourceManager.java
@@ -0,0 +1,13 @@
+package tankrotationexample.Resources;
+
+import javax.sound.sampled.Clip;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+public class ResourceManager {
+    private final static Map<String, BufferedImage> sprites = new HashMap();
+    private final static Map<String, List<BufferedImage>> animation = new HashMap<>();
+    private final static Map<String, Clip> sounds = new HashMap<>();
+
+}
