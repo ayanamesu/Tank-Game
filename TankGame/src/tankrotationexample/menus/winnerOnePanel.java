93befa5cab5692e417fb2f03b79fca2ef410969e
ayanamesu <leejacky74@gmail.com>
2023-08-07 19:37:29 -0700

    creating the jar is left

--- TankGame/src/tankrotationexample/menus/winnerOnePanel.java
+++ TankGame/src/tankrotationexample/menus/winnerOnePanel.java
@@ -6,15 +6,21 @@ import tankrotationexample.Resources.ResourceManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class winnerOnePanel extends JPanel {
     private Launcher lf;
 
+    private BufferedImage menuBackground;
+
     public winnerOnePanel(Launcher lf) {
         this.lf = lf;
+        menuBackground = ResourceManager.getSprite("menu");
+        this.setBackground(Color.BLACK);
+        this.setLayout(null);
         JButton start = new JButton("Continue");
         start.setFont(new Font("Courier New", Font.BOLD, 24));
-        start.setBounds(600, 800, 250, 50);
+        start.setBounds(150, 350, 250, 50);
         start.addActionListener((actionEvent -> this.lf.setFrame("end")));
         this.add(start);
     }
@@ -22,7 +28,16 @@ public class winnerOnePanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(ResourceManager.getSprite("tank1"), 0, 0, null);
+        g2.drawImage(this.menuBackground, 0, 0, null);
+
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Courier New", Font.BOLD, 40));
+        String message = "Tank 1 Winner!";
+        int centX = 300;
+        int buttonWidth = 250;
+        int messageX = ( centX + buttonWidth - g.getFontMetrics().stringWidth(message)) / 2;
+        int messageY = 310;
+        g.drawString(message, messageX, messageY);
     }
 
 
--- TankGame/src/tankrotationexample/menus/winnerTwoPanel.java
+++ TankGame/src/tankrotationexample/menus/winnerTwoPanel.java
@@ -5,15 +5,21 @@ import tankrotationexample.Resources.ResourceManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class winnerTwoPanel extends JPanel {
     private Launcher lf;
+    private BufferedImage menuBackground;
+
 
     public winnerTwoPanel(Launcher lf) {
         this.lf = lf;
+        menuBackground = ResourceManager.getSprite("menu");
+        this.setBackground(Color.BLACK);
+        this.setLayout(null);
         JButton start = new JButton("Continue");
         start.setFont(new Font("Courier New", Font.BOLD, 24));
-        start.setBounds(150, 300, 250, 50);
+        start.setBounds(150, 350, 250, 50);
         start.addActionListener((actionEvent -> this.lf.setFrame("end")));
         this.add(start);
     }
@@ -21,7 +27,15 @@ public class winnerTwoPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(ResourceManager.getSprite("tank2"), 0, 0, null);
+        g2.drawImage(this.menuBackground, 0, 0, null);
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Courier New", Font.BOLD, 40));
+        String message = "Tank 2 Winner!";
+        int centX = 300;
+        int buttonWidth = 250;
+        int messageX = ( centX + buttonWidth - g.getFontMetrics().stringWidth(message)) / 2;
+        int messageY = 310;
+        g.drawString(message, messageX, messageY);
     }
 
 
